15c727d098ca66b56c70178df6c5028ec0555498
namelessking <habibur.tanjin@gmail.com>
2019-07-10 22:07:05 +0200

    add PaymentDetailContext Model

---
+++ PaymentDetailRegisterAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentDetailRegisterAPI.Models
+{
+    public class PaymentDetailContext
+    {
+    }
+}
