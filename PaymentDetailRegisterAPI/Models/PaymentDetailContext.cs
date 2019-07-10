bb96456e24cb26df58a154e6fcad696217b66f61
namelessking <habibur.tanjin@gmail.com>
2019-07-10 22:36:52 +0200

    completed model for the API

--- PaymentDetailRegisterAPI/Models/PaymentDetailContext.cs
+++ PaymentDetailRegisterAPI/Models/PaymentDetailContext.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+
 
 namespace PaymentDetailRegisterAPI.Models
 {
-    public class PaymentDetailContext
+    public class PaymentDetailContext : DbContext
     {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
     }
 }
