191f73d619b0faf08b82f13a9d850eca6f2cc75c
namelessking <habibur.tanjin@gmail.com>
2019-07-10 22:19:26 +0200

    add PaymentDetail model

---
+++ PaymentDetailRegisterAPI/Models/PaymentDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentDetailRegisterAPI.Models
+{
+    public class PaymentDetail
+    {
+        public int PMId { get; set; }
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CVV { get; set; }
+    }
+}
