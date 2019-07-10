98e98faa08201eafc2b32ddbed871a5720a43c1d
namelessking <habibur.tanjin@gmail.com>
2019-07-10 22:35:03 +0200

    add DataAnnotations to PaymentDetail

--- PaymentDetailRegisterAPI/Models/PaymentDetail.cs
+++ PaymentDetailRegisterAPI/Models/PaymentDetail.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PaymentDetailRegisterAPI.Models
 {
     public class PaymentDetail
     {
+        [Key]
         public int PMId { get; set; }
+
+        [Required]
+        [Column(TypeName ="nvarchar(100)")]
         public string CardOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
         public string CardNumber { get; set; }
-        public string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }//MM/YY
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
         public string CVV { get; set; }
     }
 }
