4f865d20fce6a846809ab91ebde1ff6075214f98
namelessking <habibur.tanjin3@gmail.com>
2019-07-14 01:06:20 +0200

    ending payment-detail.component.html

--- PaymentDetailRegisterUI/src/app/app.module.ts
+++ PaymentDetailRegisterUI/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
 import { PaymentDetailListComponent } from './payment-details/payment-detail-list/payment-detail-list.component';
+import { PaymentDetailService } from './shared/payment-detail.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { PaymentDetailListComponent } from './payment-details/payment-detail-lis
     PaymentDetailListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PaymentDetailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
